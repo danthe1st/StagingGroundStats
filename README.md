237ef044f666c02cba3b42ee64175776de8c88c1
danthe1st <daniel@wwwmaster.at>
2024-09-28 14:21:30 +0200

    clarity

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Staging Ground statistics
 
-I took the last 100 pages of Staging Ground approvals from the review history, grouped these by user, filtered out inactive users and created bar charts out of it.
+I took the last 100 pages of Staging Ground approvals (state: 27.9.2024) from the review history, grouped these by user, filtered out inactive users and created bar charts out of it.
 
 The ones with "big" in the name include all users who contributed more than 5 of these approvals, ones marked "small" only include users who contributed more than 20 of these approvals.
 
 The horizontal lines represents the median.
 
-- Approval count is the total number of questions the reviewer has approved.
-- Sum is the sum of votes/views of all questions the reviewer has approved.
-- Mean refers to the mean votes/views on questions approved by the reviewer.
-- interquantile mean is the mean excluding outliers (anything smaller than the 10%th quantile or greater than the 90% quantile)
-- count positive-negative is the number of questions with a positive score minus the number of questions with a negative score approved by the reviewer
+- `mean` refers to the mean votes/views on questions approved by the reviewer.
+- `interquantile mean` is the mean excluding outliers (anything smaller than the 10%th quantile or greater than the 90% quantile)
+- `mean [positive - negative]` refers to the number of questions with a positive score minus the number of questions with a negative score normalized by the total number of approved questions
+- `ratio of questions with answers` is the amount of approved questions divided by the amount of approved questions with at least one answer
+- `sum` is the sum of votes/views of all questions the reviewer has approved.
+- `total number of approved questions` is the total number of questions the reviewer has approved.
+- ratio is the of approved questions with the attribute divided by the approval count i.e. the ratio of closed/deleted questions (or mean number of answers, I know the title doesn't fit here)
 - count is the total number of closed/deleted questions or answers to questions the reviewer has approved
-- ratio is the total count divided by the approval count i.e. the ratio of closed/deleted questions (or mean number of answers, I know the title doesn't fit here)
 
--- analyze.py
+++ analyze.py
@@ -20,24 +20,36 @@ def op_plot(ax, user_arrays, op, name):
     ax.tick_params(axis='x', labelrotation=-90)
     ax.axhline(np.median(values))
 
-def multiplot(user_arrays, name):
-    fig, ax = plt.subplots(5)
+if True:
+    FILE_SUFFIX="_small.png"
+    MIN_QUESTIONS=20
+else:
+    FILE_SUFFIX="_big.png"
+    MIN_QUESTIONS=5
+
+def multiplot(user_arrays, name, include_mean_pos_neg=True, mean_pos_neg_name="mean [positive - negative]"):
+    fig, ax = plt.subplots(4 + int(include_mean_pos_neg))
+    fig.set_size_inches(25, 15)
     fig.suptitle(name, fontsize=14)
-    plt.subplots_adjust(hspace=2)
-    op_plot(ax[0], user_arrays, len, "approval count")
-    op_plot(ax[1], user_arrays, np.sum, "sum")
-    op_plot(ax[2], user_arrays, np.mean, "mean")
-    op_plot(ax[3], user_arrays, inter_quantile_mean, "inter quantile mean")
-    op_plot(ax[4], user_arrays, lambda x: ((np.array(x)>0).sum()-(np.array(x)<0).sum())/len(x), "count positive - negative")
+    plt.subplots_adjust(hspace=2.5)
+    op_plot(ax[-1], user_arrays, len, "total number of approved questions")
+    op_plot(ax[-2], user_arrays, np.sum, "sum")
+    op_plot(ax[0], user_arrays, np.mean, "mean")
+    op_plot(ax[1], user_arrays, inter_quantile_mean, "inter quantile mean")
+    if include_mean_pos_neg:
+        op_plot(ax[2], user_arrays, lambda x: ((np.array(x)>0).sum()-(np.array(x)<0).sum())/len(x), mean_pos_neg_name)
+    plt.savefig(f"{name}{FILE_SUFFIX}")
     plt.show()
 
 def boolplot(user_arrays, name):
     fig, ax = plt.subplots(3)
+    fig.set_size_inches(25, 15)
     fig.suptitle(name, fontsize=14)
-    plt.subplots_adjust(hspace=2)
-    op_plot(ax[0], user_arrays, len, "approval count")
+    plt.subplots_adjust(hspace=2.5)
+    op_plot(ax[2], user_arrays, len, "total number of approved questions")
     op_plot(ax[1], user_arrays, sum, "count")
-    op_plot(ax[2], user_arrays, lambda x: sum(x)/len(x), "ratio")
+    op_plot(ax[0], user_arrays, lambda x: sum(x)/len(x), "ratio")
+    plt.savefig(f"{name}{FILE_SUFFIX}")
     plt.show()
 
 def inter_quantile_mean(values):
@@ -51,7 +63,7 @@ if __name__ == "__main__":
         raw_data = json.load(fh)
     stats = {}
     for user_url, user_data in raw_data.items():
-        if len(user_data)>20:
+        if len(user_data)>MIN_QUESTIONS:
             user_url_split = user_url.split("/")
             user_name = user_url_split[-1]
             user_id = user_url_split[-2]
@@ -66,13 +78,8 @@ if __name__ == "__main__":
                     if user_url not in stats[k]:
                         stats[k][user_url]=[]
                     stats[k][user_url].append(v)
-    #print(json.dumps(stats))
     multiplot(stats["votes"], "score")
-    boolplot(stats["answers"], "answers")
-    multiplot(stats["views"], "views")
+    multiplot(stats["answers"], "answers", mean_pos_neg_name="ratio of questions with answers")
+    multiplot(stats["views"], "views", include_mean_pos_neg=False)
     boolplot(stats["closed"], "closed")
     boolplot(stats["deleted"], "deleted")
-    #op_plot(ax, stats["votes"], np.sum, "sum")
-    #op_plot(ax, stats["votes"], np.mean, "mean")
-    #op_plot(ax, stats["votes"], inter_quantile_mean, "inter quantile mean")
-    #op_plot(ax, stats["votes"], lambda x: ((np.array(x)>0).sum()-(np.array(x)<0).sum())/len(x), "positive - negative")
